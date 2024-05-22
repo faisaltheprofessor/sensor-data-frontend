b994ea145b87cbd48c6a2cad1e84b239357106d7
faisaltheprofessor <faisalfazily@gmail.com>
2024-05-22 21:36:35 +0200

    add form dialogue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,25 @@
+import { FormDialogue } from "@/components/FormDialogue";
 import SensorData from "@/components/SensorData";
-import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { Switch } from "@/components/ui/switch"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-top w-full">
-     
 
-       <div className="w-[50%] mx-auto mt-20">
-        <div className="flex items-end">
-          <Button>Add Data</Button>
+
+      <div className="w-[50%] mx-auto mt-20 ">
+        <div className="flex justify-between mb-4">
+          <div className="flex gap-x-2">
+            Descending <Switch title="Descending" />
+          </div>
+          <FormDialogue />
         </div>
-        <SensorData />
-       </div>
+        <Separator />
+        <SensorData desc={true} />
+      </div>
 
-     
+
     </main>
   );
 }
--- src/components/SensorData.tsx
+++ src/components/SensorData.tsx
@@ -2,15 +2,19 @@
 import React, { useState, useEffect } from 'react';
 import { TableHeader, TableRow, TableHead, TableBody, TableCell, Table } from './ui/table';
 import Pagination from './pagination';
-
-const SensorData = () => {
+import { Separator } from './ui/separator';
+interface Props {
+  descending?: boolean
+}
+const SensorData = ({ descending = false }: Props) => {
   const [sensorData, setSensorData] = useState([]);
-  const [links, setLinks] = useState({}); // Changed links to object
+  const [links, setLinks] = useState({});
   const [limit, setLimit] = useState(10);
   const [page, setPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(0); // Added state for total pages
+  const [totalPages, setTotalPages] = useState(0);
 
-  const [url, setUrl] = useState(`http://localhost:8000/sensors/data?limit=${limit}&page=${page}`);
+  let endpoint = descending ? `http://localhost:8000/sensors/data?limit=${limit}&page=${page}&desc=true` : `http://localhost:8000/sensors/data?limit=${limit}&page=${page}`
+  const [url, setUrl] = useState(endpoint);
 
   const fetchSensorData = async () => {
     try {
@@ -37,7 +41,7 @@ const SensorData = () => {
   const updateTable = (limit: number, page: number) => {
     setLimit(limit);
     setPage(page);
-    setUrl(`http://localhost:8000/sensors/data?limit=${limit}&page=${page}`);
+    descending ? setUrl(`http://localhost:8000/sensors/data?limit=${limit}&page=${page}&desc=true`) : setUrl(`http://localhost:8000/sensors/data?limit=${limit}&page=${page}`)
     fetchSensorData();
   };
 
@@ -63,7 +67,7 @@ const SensorData = () => {
           ))}
         </TableBody>
       </Table>
-
+        <Separator />
       <Pagination
         perPage={limit}
         totalPages={totalPages} // Pass totalPages as a prop
@@ -73,6 +77,7 @@ const SensorData = () => {
         first={links.first}
         last={links.last}
         action={updateTable}
+        desc={true}
       />
     </>
   );
